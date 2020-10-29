b94cb4aef73097f300ea487cfd5e673a2ceb1da5
Matias Antiguay <71038360+manticr@users.noreply.github.com>
2020-10-29 08:20:12 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hashmap.h"
-#include "hashmap.c"
 #include "lectura_csv.c"
 #include "menu.c"
 
@@ -10,30 +9,37 @@ typedef struct
 {
     char* nombre;
     HashMap* distancias;
-} ciudad; //Estructura de cada ciudad, tiene su nombre y una distancia
+} ciudad; //Estuctura de cada ciudad (Nombre y distancia)
 
-ciudad * crearCiudad() //Funcion que crea una ciudad
+ciudad * crearCiudad() //Funcion para crear una ciudad
 {
-    ciudad* input = (ciudad *) calloc (1, sizeof(ciudad)); 
-    input->nombre = (char *) calloc (20, sizeof(ciudad));
+    ciudad* input = (ciudad *) calloc (1, sizeof(ciudad)); //Se asigna memoria a la ciudad
+    input->nombre = (char *) calloc (20, sizeof(char));
     input->distancias = createMap(50);
-    return input;
+    return input; //Se retorna la ciudad creada
 }
-void agregaInfo (HashMap* ciudades, char* ciudad1, char* ciudad2, int distancia)
-{
-    ciudad* c1 = searchMap(ciudades, ciudad1); 
-    ciudad* c2 = searchMap(ciudades, ciudad2);
+/*
+typedef struct{
+   char ciudad[30];
+   int dist; 
+   Node* prev; 
+}Node;*/
 
-    if(c1 == NULL) //Si la ciudad1 no se encuentra:
+void agregaInfo (HashMap* map, char* ciudad1, char* ciudad2, int distancia) //Funcion para agregar ciudades al un mapa
+{
+    ciudad* c1 = searchMap(map, ciudad1); //Se buscan en el mapa las ciudades 
+    ciudad* c2 = searchMap(map, ciudad2);
+
+    if(c1 == NULL) 
     {
-        c1 = crearCiudad(); //Se crea una ciudad respecto a c1
-        insertMap(ciudades, ciudad1, c1); //Se inserta la ciudad1 en el mapa de ciudades
+        c1 = crearCiudad();
+        insertMap(map, ciudad1, c1);
     }
 
     if(c2 == NULL) 
     { 
-        c2 = crearCiudad(); //Se crea una ciudad c2
-        insertMap(ciudades, ciudad2, c2);
+        c2 = crearCiudad();
+        insertMap(map, ciudad2, c2);
     }
 
     insertMap(c1->distancias, ciudad2, &distancia);
@@ -42,18 +48,18 @@ void agregaInfo (HashMap* ciudades, char* ciudad1, char* ciudad2, int distancia)
 
 int main()
 {
-    HashMap* map = createMap(50);
+    HashMap* map = createMap(50); //Se crea el mapa
     char* ciudad1;
     char* ciudad2;
     int distancia = 0;
-    FILE* Ciudades = fopen("distances.txt", "r");
+    FILE* Ciudades = fopen("distances.csv", "r"); //Se abre el archivo que contiene las ciudades y sus distancias
     char line[30];
     while(fgets(line, 29, Ciudades) != NULL)
     {
         ciudad1 = (char *) get_csv_field(line, 0);
         ciudad2 = (char *) get_csv_field(line, 1);
         distancia = atoi(get_csv_field(line, 2));
-        agregaInfo(map, ciudad1, ciudad2, distancia);
+        agregaInfo(map, ciudad1, ciudad2, distancia); //Se van agregando las ciudades leidas al mapa correspondiente
     }
 
     fclose(Ciudades);
@@ -63,14 +69,15 @@ int main()
 }
 
 /* FUNCIONES */
-void Importar(char* nombre, HashMap* map) //Esta funcion importa ciudades desde un archivo ingresado por el usuario 
+
+void Importar(char* nombre, HashMap* map) //Funcion que importa ciudades desde un archivo ingresado por el usuario 
 {
     FILE* input;
     char nombre_Archivo[20];
     snprintf(nombre_Archivo, sizeof(nombre_Archivo), "%s%s", nombre, ".txt");
     input = fopen(nombre_Archivo, "r");
     int cont = 0;
-    if(input == NULL)
+    if(input == NULL) //Caso de que el archivo a leer no se encuentre
     {
         printf("No existe el archivo\n");
         return;
@@ -92,6 +99,7 @@ void Importar(char* nombre, HashMap* map) //Esta funcion importa ciudades desde 
     free(input);
     printf("Archivo importado exitosamente!\n");
 }
+ 
 
 void CiudadMasCercana (HashMap* map, char* ciudad)
 {
@@ -99,12 +107,20 @@ void CiudadMasCercana (HashMap* map, char* ciudad)
   char mas_cercana[10];
 
   int flag = 0;
+  int cont = 0;
   int pos = 0;
-  while(map->buckets[pos] != NULL)
+  while(map != NULL)
   {
-    if(strcmp(map->buckets[pos]->key, ciudad) == 0)
-    {
-      flag++;
+    if(firstMap(HashMap* map) != NULL){
+      if(strcmp(map->buckets[cont]->key, ciudad) == 0)
+      {
+        flag++;
+        break;
+      }
+      cont++;
+    }
+    else{
+      printf("1");
       break;
     }
   }
@@ -114,35 +130,49 @@ void CiudadMasCercana (HashMap* map, char* ciudad)
     printf("No se encontro la ciudad en tu GPS\n");
     return;
   }
-
+  
   //Algoritmo de Dijkstra
 
 }
 
 void CrearRuta(HashMap* map, char* origen)
 {
-  //Se revisa que la ciudad de origen este en el mapa
-  int flag = 0;
-  int pos = 0;
-  while(map->buckets[pos] != NULL)
-  {
-    if(strcmp(map->buckets[pos]->key, ciudad) == 0)
+    ciudad* aux = searchMap(map, origen); //se crea un auxiliar a partir de la ciudad ingresada
+    if(aux == NULL)
     {
-      flag++;
-      break;
+        printf("La ciudad de origen no se encuentra almacenada en tu GPS\n"); //Caso de que no se encuentre la ciudad
+        return;
     }
-  }
 
-  if(flag == 0)
-  {
-    printf("No se encontro la ciudad de origen en tu GPS\n");
-    return;
-  }
+    while(1) //Si la ciudad se encuentra:
+    {
+        ciudad* aux_distancias = firstMap(aux->distancias);
+        while(aux_distancias != NULL)
+        {
+            printf("%s\n", aux_distancias->nombre);
+            aux_distancias = nextMap(aux->distancias);
+        }
+        char validador[3];
+        printf("¿Deseas continuar tu ruta? (INGRESE 'Si' o 'No' respetando mayusculas)\n");
+        fflush(stdin);
+        scanf("%s", validador);
+        if(strcmp(validador, "No") == 0) return;
+        else
+        {
+            char ciudad_sgte[10];
+            printf("¿A que ciudad deseas ir ahora?\n");
+            fflush(stdin);
+            scanf("%s", ciudad_sgte);
+            aux = searchMap(map, ciudad_sgte);
+        }
 
-  //A partir de una ciudad se va mostrando por pantalla los nombres de las ciudades adyacentes y el usuario va ingresando el nombre de la ciudad a la cual quiere ir
+    }    
 }
 
 void RutaOptima(HashMap* map, char* origen, char* destino)
 {
   //Algoritmo de Dijkstra
+  
+
+
 }
