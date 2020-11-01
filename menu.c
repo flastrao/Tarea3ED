02a557173bdf522a74646d60255a7b9e558d7b90
Matias Antiguay <71038360+manticr@users.noreply.github.com>
2020-11-01 18:12:21 -0300

    Add files via upload

--- menu.c
+++ menu.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
-void Importar(char* nombre, HashMap* map);
+void Importar(HashMap* map, char* nombre);
+void CiudadMasCercana(HashMap* map, char* nombre);
+void CrearRuta(HashMap* map, char* origen);
+void mostrartodo(HashMap* map);
 
 int menu (HashMap* map)
 {
     char opcion;
-    int flag = -1;
+    int flag = -1; //Variable que mantiene el menu en uso hasta que el usuario indica que no desea realizar otra accion
     printf("Bienvenido a tu GPS\n");
 
     while(flag != 0)
     {
         printf("\n");
         printf("¿Que deseas hacer?\n a) Buscar la ciudad mas cercana\n b) Crear tu propia ruta\n c) Encontrar la ruta mas corta\n d) Encontrar el mejor circuito\n e) Importar ciudades nuevas a tu GPS\n \n Si no quieres hacer nada presiona ENTER\n");
-        fflush(stdin);
+        fflush(stdin); //Se limpia stdin para que no guarde basura
         scanf("%c", &opcion);
         switch (opcion)
         {
             case 'a':
             {
                 char nombre[10];
-                printf("Ingresa el nombre de la ciudad de la cual quieres saber la ciudad mas cercana\n");
+                printf("Ingresa el nombre de la ciudad de la cual quieres saber la ciudad mas cercana (Respete mayusculas e ignore espacios en caso de ser necesitados)\n");
                 fflush(stdin);
                 scanf("%s", nombre);
                 CiudadMasCercana(map, nombre);
@@ -28,23 +31,23 @@ int menu (HashMap* map)
             case 'b':
             {
               char origen[10];
-              printf("Ingresa el nombre de la ciudad donde quieres comenzar tu ruta\n");
+              printf("Ingresa el nombre de la ciudad donde quieres comenzar tu ruta (Respete mayusculas e ignore espacios en caso de ser necesitados)\n");
               fflush(stdin);
               scanf("%s", origen);
-              CrearRuta(map, origen, destino);
+              CrearRuta(map, origen);
               break;
             }
             case 'c':
             {
               char origen[10];
               char destino[10];
-              printf("Ingresa el nombre de la ciudad donde quieres comenzar tu ruta\n");
+              printf("Ingresa el nombre de la ciudad donde quieres comenzar tu ruta (Respete mayusculas e ignore espacios en caso de ser necesitados)\n");
               fflush(stdin);
               scanf("%s", origen);
-              printf("Ingresa el nombre de la ciudad donde quieres terminar tu ruta\n");
+              printf("Ingresa el nombre de la ciudad donde quieres terminar tu ruta (Respete mayusculas e ignore espacios en caso de ser necesitados)\n");
               fflush(stdin);
               scanf("%s", destino);
-              RutaOptima(map, origen, destino);
+              //RutaOptima(map, origen, destino);
               break;
             }
             case 'e':
@@ -53,7 +56,7 @@ int menu (HashMap* map)
                 printf("Ingrese el nombre del archivo a importar (Sin formato)\n");
                 fflush(stdin);
                 scanf("%s", archivo);
-                Importar(archivo, map);
+                Importar(map, archivo);
                 break;
             }
             case '\n':
@@ -63,8 +66,9 @@ int menu (HashMap* map)
             }
             default:
             {
-                printf("Entrada no valida\n");
-                printf("\n");
+                //printf("Entrada no valida\n");
+                //printf("\n");
+                mostrartodo(map);
                 break;
             }        
         }
